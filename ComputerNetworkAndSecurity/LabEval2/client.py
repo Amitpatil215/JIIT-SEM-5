a9198a47385fa287cd4e7aa850ae85c7df57a78a
Amit Patil <amitgpatil215@gmail.com>
2021-11-12 11:51:22 +0530

    cns lab eval 2

---
+++ ComputerNetworkAndSecurity/LabEval2/client.py
@@ -0,0 +1,35 @@
+import socket
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+
+print("Enter First input")
+input1=input()
+
+print("Enter Second input")
+input2 = input()
+
+send(input1)
+send(input2)
+
+input()
+
+send(DISCONNECT_MESSAGE)
